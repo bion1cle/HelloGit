bcbb9afebdf55e44bcaaf0c9ccb53d8cea07be38
bion1cle <bheinrich@gmx.net>
2014-06-19 14:45:35 +0200

    Update Program.cs

--- HelloGitWorld/Hello/Program.cs
+++ HelloGitWorld/Hello/Program.cs
@@ -11,6 +11,7 @@ namespace Hello
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine(Environment.CurrentDirectory.ToString());
         }
     }
 }
